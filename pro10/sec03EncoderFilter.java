462e2a8009f771b97b0c80e1f963271b6edffbaf
Jaewoon-Lee <88029760+Jaewoon-Lee@users.noreply.github.com>
2021-08-12 16:23:50 +0900

    utf-8로 변환해주는 필터

---
+++ pro10/sec03EncoderFilter.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+@WebFilter("/*")
+public class EncoderFilter implements Filter {
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		long begin = System.currentTimeMillis(); //시작시간
+		System.out.println("필터사용");
+		request.setCharacterEncoding("utf-8");
+		chain.doFilter(request, response);
+		
+		long end = System.currentTimeMillis(); //끝난 시간
+		System.out.println("작업시간" + (end-begin)+"ms");
+	}
+}
