9b255891f4daa28338fc3f046deffb2a52ae7273
Jaewoon-Lee <88029760+Jaewoon-Lee@users.noreply.github.com>
2021-08-12 16:23:14 +0900

    utf-8 변환해주는 필터 테스트

---
+++ pro10/sec03/LoginTest.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/login")
+public class LoginTest extends HttpServlet {
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		
+		String user_id = request.getParameter("user_id");
+		String user_pw = request.getParameter("user_pwd");
+		out.print("<html><body>");
+		out.print("ID : " + user_id + "<br>");
+		out.print("PW : " + user_pw + "<br>");
+		out.print("</body></html>");
+	}
+}
