b60cdbef75e5c9376192afbccf17065b14396956
Jaewoon-Lee <88029760+Jaewoon-Lee@users.noreply.github.com>
2021-08-19 12:46:26 +0900

    SessionTest servlet파일
    세션,어플리케이션,리퀘스트,아웃 객체 사용예제

---
+++ pro12/sec07/SessionTest.java
@@ -0,0 +1,26 @@
+package sec01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebServlet("/sess")
+public class SessionTest extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		HttpSession session = request.getSession();
+		session.setAttribute("name", "이순신");
+		out.println("<html><body>");
+		out.println("<a href='/pro12/session1.jsp'>다음 페이지로 이동하기</a>");
+		out.println("</body></html>");
+	}
+}
